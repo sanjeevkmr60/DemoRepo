13c07d40cc757929e87bbf792206ae098793e89c
Sanjeevb <sanjeev.bandari@wavemaker.com>
2019-11-08 15:41:24 +0530

    Added Encapsulation and StringOnlyDigits

---
+++ src/main/java/com/qa/Sample/EncapsulationEmpDataConcept.java
@@ -0,0 +1,51 @@
+package com.qa.Sample;
+
+public class EncapsulationEmpDataConcept {
+
+
+    //Encapsulation is also called datahiding
+    //how to implement Encapsulation
+    //private data varibles
+
+    //private data varaibles are not accessed directly from outside the class
+     private int ssn;
+     private String empName;
+     private int empAge;
+
+    //getter and settet methods
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(final String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpAge() {
+        return empAge;
+    }
+
+    public void setEmpAge(final int empAge) {
+        this.empAge = empAge;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(final int ssn) {
+        this.ssn = ssn;
+    }
+
+
+    public static void main(String args[]){
+        EncapsulationEmpDataConcept emp=new EncapsulationEmpDataConcept();
+        emp.setSsn(12345);
+        emp.setEmpName("Sanjeev");
+        emp.setEmpAge(27);
+        System.out.println("emp ssn  :"+emp.getSsn());
+        System.out.println("emp empName  :"+emp.getEmpName());
+        System.out.println("emp Age  :"+emp.getEmpAge());
+    }
+}
---
+++ src/main/java/com/qa/Sample/StringonlyConatinDigits.java
@@ -0,0 +1,43 @@
+package com.qa.Sample;
+
+public class StringonlyConatinDigits {
+
+ public static boolean isEmpty(CharSequence cs){
+
+     return cs==null || cs.length()==0;
+ }
+
+ public static boolean isNumeric(CharSequence cs){
+     if(isEmpty(cs)){
+         return false;
+     }
+      int len=cs.length();
+     for(int i=0;i<cs.length();i++){
+         if(!Character.isDigit(cs.charAt(i))){
+             return false;
+         }
+     }
+     return true;
+ }
+
+
+ public static void main(String args[])
+ {
+     System.out.println(isNumeric(null));
+     System.out.println(isNumeric(""));
+     System.out.println(isNumeric(" "));
+     System.out.println(isNumeric("a"));
+     System.out.println(isNumeric("1"));
+     System.out.println(isNumeric("123"));
+     System.out.println(isNumeric("123456"));
+     System.out.println(isNumeric("testing"));
+     System.out.println(isNumeric("test123"));
+     System.out.println(isNumeric("123test"));
+     System.out.println(isNumeric("2.3"));
+     System.out.println(isNumeric("+1223"));
+     System.out.println(isNumeric("-123"));
+     System.out.println(isNumeric("0"));
+     System.out.println(isNumeric("\\u0967\\u0968\\u0969"));
+     System.out.println(isNumeric("1 1 1"));
+ }
+}
