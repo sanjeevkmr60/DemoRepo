26c6c5896e73f0fe9429cb9a8e37c7b2c92e36eb
Sanjeevb <sanjeev.bandari@wavemaker.com>
2019-11-28 14:53:54 +0530

    Date related Scenarios

---
+++ src/main/java/com/qa/Sample/DateDemo.java
@@ -0,0 +1,40 @@
+package com.qa.Sample;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDemo {
+
+    public static void main(String args[]){
+        Date date=new Date();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+        System.out.println("Current Date: " + simpleDateFormat.format(date));
+
+        System.out.println("****************************");
+        // display time and date
+        String str = String.format("Current Date/Time : %tc", date );
+        System.out.printf(str);
+
+        System.out.println("/n");
+        System.out.println("****************************");
+        // display time and date
+        System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
+
+        // display formatted date
+        System.out.printf("%s %tB %<te, %<tY", "Due date:", date);
+
+        try {
+            long start = System.currentTimeMillis( );
+            System.out.println(new Date( ) + "\n");
+
+            Thread.sleep(5*60*10);
+            System.out.println(new Date( ) + "\n");
+
+            long end = System.currentTimeMillis( );
+            long diff = end - start;
+            System.out.println("Difference is : " + diff);
+        } catch (Exception e) {
+            System.out.println("Got an exception!");
+        }
+    }
+}
