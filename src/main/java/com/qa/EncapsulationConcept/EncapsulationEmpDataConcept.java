4ea37d62fe1223734d4268eb07ae2474c8589c15
Sanjeevb <sanjeev.bandari@wavemaker.com>
2020-02-19 17:17:42 +0530

    Added documenttation to Abstraction,Interface,Encapsulation,Word count_Program

--- src/main/java/com/qa/EncapsulationConcept/EncapsulationEmpDataConcept.java
+++ src/main/java/com/qa/EncapsulationConcept/EncapsulationEmpDataConcept.java
@@ -7,9 +7,23 @@ public class EncapsulationEmpDataConcept {
       Private data variables
       Private data variables are not accessed directly from outside the class.
 
+      Java Encapsulation:
+
+Encapsulation is a mechanism where we collect code (methods) as a unit while working on data (variables) and data.
+In java encapsulation, the variables of one class will be hidden from other sections, and can be accessed only
+through the methods of their current class, therefore, this is also known as data hiding.
+In order to achieve the encapsulation, we need to declare the variable of one class as
+private and to provide a public setter and a getter method for modifying and viewing variable values.
+
+To get encapsulation in Java:
+
+Announce a class variable as a private.
+Provide public setter and getter methods for modifying and viewing variable values.
+It gives you control over the data.
+
     Advantages:
     1.Data hiding.
-    2.Increased Flexibility.
+    2.Increased Flexibility:Based on our requirement, we can only read or write only the variables of the class.
     3.Resuabilitiy
     4.Testing code is easy.
      */
